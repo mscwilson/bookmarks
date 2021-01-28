69b26033dac19a2982c299166438ba30b8380169
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-28 14:36:10 +0000

    tidied up the webhelper

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,6 +1,5 @@
 def insert_three_bookmarks
-  connection = PG.connect(dbname: 'bookmark_manager_test')
-  connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-  connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-  connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+  Bookmark.create('http://www.makersacademy.com')
+  Bookmark.create('http://www.destroyallsoftware.com')
+  Bookmark.create('http://www.google.com')
 end
