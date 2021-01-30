15b145b5a6be01398c5cd4e3b17c32803427829a
Miranda Wilson <mirandascwilson@gmail.com>
2021-01-30 18:20:46 +0000

    signing in completed

--- lib/user.rb
+++ lib/user.rb
@@ -20,7 +20,10 @@ class User
 
   def self.authenticate(email:, password:)
     result = DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'")
-    User.new(result[0]['id'], result[0]['email'])
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]["password"]) == password
+
+    User.new(id: result[0]['id'], email: result[0]['email'])
   end
 
   attr_reader :id, :email
